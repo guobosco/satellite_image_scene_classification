5c80a236b23b351803c8c9f97935ef1a69cad8b5
guobosco <guobosco@163.com>
2019-01-09 14:57:43 +0800

    resize程序

--- img_preprocessing/resize_256to224.py
+++ img_preprocessing/resize_256to224.py
@@ -1,11 +1,24 @@
 import cv2
+import os
 
-img256 = cv2.imread('/Users/mac/Documents/GitHub'
-                 '/satellite_image_scene_classification/img_preprocessing'
-                 '/test_imgs/agricultural00.tif')
+#源文件路径
+read_path = '/Users/mac/Documents/GitHub' \
+            '/satellite_image_scene_classification/UCMerced_LandUse'
+#写入路径
+write_path = '/Users/mac/Documents/GitHub' \
+             '/satellite_image_scene_classification/UCMerced_LandUse(224)'
+#得到文件名字符串列表img_list_name
+img_list =  os.listdir(read_path)
+img_list_name = []
+for i in range(0,len(img_list)):
+    img_list_name.append(os.path.basename(img_list[i]))
 
-img224 = cv2.resize(img256,(224,224))
+#逐个对每个图像文件处理大小resize
+for img_name in img_list_name:
+    if img_name[-4:-1] == '.tif':       #确保是以.tif结尾的图像文件
+        img256 = cv2.imread(read_path + "/" + img_name)
+        img224 = cv2.resize(img256, (224, 224))         #本程序核心代码
+        cv2.imwrite(write_path+ "/" + img_name,img224)
 
-cv2.imwrite('/Users/mac/Documents'
-            '/GitHub/satellite_image_scene_classification/img_preprocessing'
-            '/test_imgs/agricultural00.png',img224)
+
+
